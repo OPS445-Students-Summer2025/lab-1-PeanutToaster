0d0ddaeb28abe813fa5ce9805b353480b89097a0
Ricky Tang <rtang41@myseneca.ca>
2025-05-13 08:49:06 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# 5th investigation for lab1
+# Ricky Tang - 104448246
+# maths with python
+
+x = 10
+y = 2
+z = 5
+a = (x + y * z)
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(a))
